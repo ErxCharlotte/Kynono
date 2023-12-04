48acd3eb958dda7d9c651246d9eb118950f68e13
ErxCharlotte <878658607@qq.com>
2023-12-05 01:18:05 +1100

    Site updated: 2023-12-05 01:18:05

--- js/cursor.js
+++ js/cursor.js
@@ -1,36 +1,37 @@
-var cursorFrames = {
-    default: [
-        '/cursors/default/1.png', '/cursors/default/2.png', '/cursors/default/3.png', '/cursors/default/4.png', '/cursors/default/5.png', '/cursors/default/6.png', '/cursors/default/7.png', '/cursors/default/8.png'
-    ],
-    pointer: [
-        '/cursors/text/1.png', '/cursors/text/2.png', '/cursors/text/3.png', '/cursors/text/4.png', '/cursors/text/5.png', '/cursors/text/6.png', '/cursors/text/7.png', '/cursors/text/8.png', '/cursors/text/9.png', '/cursors/text/10.png', '/cursors/text/11.png', '/cursors/text/12.png', '/cursors/text/13.png'
-    ],
-};
-
-var currentCursorState = 'default';
-var cursorInterval;
-
-function setCursorState(state) {
-    if (cursorFrames[state]) {
-        clearInterval(cursorInterval);
-        currentFrame = 0; // 重置当前帧
-        currentCursorState = state;
-        cursorInterval = setInterval(function() {
-            cursor.style.backgroundImage = 'url(' + cursorFrames[state][currentFrame] + ')';
-            currentFrame = (currentFrame + 1) % cursorFrames[state].length;
-        }, 100); // 根据需要调整间隔时间
-    }
-}
-
 document.addEventListener('DOMContentLoaded', function() {
     var cursor = document.getElementById('animated-cursor');
-    setCursorState('default'); // 设置初始状态
 
-    document.querySelectorAll('a').forEach(function(el) {
-        el.addEventListener('mouseenter', function() { setCursorState('pointer'); });
-        el.addEventListener('mouseleave', function() { setCursorState('default'); });
+    var defaultCursorImages = ['/cursors/default/1.png', '/cursors/default/2.png', '/cursors/default/3.png', '/cursors/default/4.png', '/cursors/default/5.png', '/cursors/default/6.png', '/cursors/default/7.png', '/cursors/default/8.png']; // 光标帧的路径
+    var textCursorImages = ['/cursors/text/1.png', '/cursors/text/2.png', '/cursors/text/3.png', '/cursors/text/4.png', '/cursors/text/5.png', '/cursors/text/6.png', '/cursors/text/7.png', '/cursors/text/8.png', '/cursors/text/9.png', '/cursors/text/10.png', '/cursors/text/11.png', '/cursors/text/12.png', '/cursors/text/13.png'];
+    
+    
+    var currentFrame = 0;
+
+    var cursorImages = defaultCursorImages;
+
+    function updateCursor() {
+        cursor.style.backgroundImage = 'url(' + cursorImages[currentFrame] + ')';
+        currentFrame = (currentFrame + 1) % cursorImages.length;
+    }
+
+    setInterval(updateCursor, 100); // 每 100ms 更换一次帧
+    
+    document.addEventListener('mousemove', function(e) {
+        cursor.style.left = (e.clientX + window.scrollX) + 'px';
+        cursor.style.top = (e.clientY + window.scrollY) + 'px';
+        cursor.style.display = 'block';
     });
 
-    // ... 监听其他事件以设置其他状态
-});
+    document.addEventListener('selectionchange', function() {
+        var selection = window.getSelection();
+        
+        if (selection && selection.toString().trim() !== '') {
+            // 用户选择了文本，触发事件，可以在这里添加相应的处理代码
+            cursorImages = textCursorImages; // 切换到文本光标
+        } else {
+            // 用户没有选择文本，不触发事件，可以在这里添加相应的处理代码
+            cursorImages = defaultCursorImages; // 切换回默认光标
+        }
+    });
 
+});
